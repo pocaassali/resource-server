7c495cc9a98aa5cbc185fcd5d0983146de3b3387
aassalax <aassali.dev@gmail.com>
2025-01-28 20:49:53 +0100

    defined all mvp endpoints

--- src/main/kotlin/com/poc/resourceserver/infrastructure/api/PersonController.kt
+++ src/main/kotlin/com/poc/resourceserver/infrastructure/api/PersonController.kt
@@ -1,13 +1,35 @@
 package com.poc.resourceserver.infrastructure.api
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.poc.resourceserver.core.domain.model.Person
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/svc/resources/person")
 class PersonController {
 
     @GetMapping
-    fun greeting() = "Hello World"
+    fun getPersons() : ResponseEntity<List<Person>>{
+        return ResponseEntity.ok(emptyList())
+    }
+
+    @GetMapping("/{id}")
+    fun getPerson(@PathVariable id: String) : ResponseEntity<Person?>{
+        return ResponseEntity.ok(null)
+    }
+
+    @PostMapping
+    fun createPerson() : ResponseEntity<Person?>{
+        return ResponseEntity.ok(null)
+    }
+
+    @PutMapping("/{id}")
+    fun updatePerson(@PathVariable id: String) : ResponseEntity<Person?>{
+        return ResponseEntity.ok(null)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletePerson(@PathVariable id: String) : ResponseEntity<Person?>{
+        return ResponseEntity.ok(null)
+    }
 }
